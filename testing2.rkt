dac076a779779e49b0e05c3b83ac4e51232e5af1
shanelee2027 <shane.lee@yale.edu>
2023-12-13 12:43:39 -0500

    tc-201 practice in hw6, edits to testing2

--- testing2.rkt
+++ testing2.rkt
@@ -1,23 +1,13 @@
 #lang racket
-(struct point (x y))
-(define (point-add point1 point2)
-  (cond
-    [(and (point? point1) (point? point2))
-     (point (+ (point-x point1) (point-x point2)) (+ (point-y point1) (point-y point2)))]
-    [else
-     null]))
-
-(define (add-one lst)
-  (cond
-    [(empty? lst)
-     '()]
-    [(number? (first lst))
-     (cons (+ 1 (first lst)) (add-one (rest lst)))]
-    [else
-     (cons (add-one (first lst)) (add-one (rest lst)))]))
-
-(define (test)
-  (let*
-      [(var1 2)
-       (var2 (+ 1 var1))]
-    (+ var1 var2)))
+(define (phonebook name)
+  (let
+      [(contacts '())]
+    (lambda (cmd . args)
+      (case cmd
+        [(add!)
+         (set! contacts (cons (first args) contacts))]
+        [(search!)
+         (ormap (lambda (x) (equal? x (first args)))
+                contacts)]
+        [(display!)
+         contacts]))))
